b07aa5ecfc0c8a1e762d3a8dafd53187a63aa0c0
Faisal <aryuical@gmail.com>
2019-11-14 21:04:57 +0700

    Finishing roomlist

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from "../apis/firestore";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    rooms: []
   },
   mutations: {
+    ROOMS(state, payload) {
+      state.rooms = payload
+    }
   },
   actions: {
+    getDataRooms({
+      commit
+    }) {
+      let roomsTemp = [];
+      db.collection("rooms").onSnapshot(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          let obj = {
+            id: doc.id
+          }
+          obj.members = doc.data().members
+          obj.total = obj.members.length
+          obj.status = doc.data().status
+          obj.startGame = doc.data().startGame
+          roomsTemp.push(obj);
+        })
+        commit("ROOMS", roomsTemp);
+      })
+    },
+
+    createRoom({
+      commit
+    }, payload) {
+      db.collection("rooms").doc(payload).set({
+        members: [],
+        status: true,
+      })
+
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
